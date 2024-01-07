97705c3140829e6e941cf5969b08d5bf87c081c7
mordico25 <mordico25@gmail>
2024-01-07 05:41:24 +0200

    fix

--- src/app/services/converter.service.ts
+++ src/app/services/converter.service.ts
@@ -31,11 +31,12 @@ export class ConverterService {
           const now = new Date();
           now.setSeconds(0);
           now.setMilliseconds(0);
-          const currencyValue = this.currencyPipe.transform(value,to)
+          const currencyValue = `${value.toFixed(3)} ${to}` //this.currencyPipe.transform(value,to)
           if(!this.history.some(value=> value.amount==amount&&value.date==now&&value.from==from&&value.to==to)){
             const newValueForHistory = { date: now, value:currencyValue, from, to, amount }
             this.history.push(newValueForHistory)
-            this.lastValue = currencyValue;
+            //this.currencyPipe.transform(amount,from)
+            this.lastValue = `${amount} ${from} = ${currencyValue}` ;
 
             let lastHistory = JSON.parse(localStorage.getItem('history') as any);
             localStorage.setItem('history',JSON.stringify([...(lastHistory?lastHistory:[]),newValueForHistory]))
